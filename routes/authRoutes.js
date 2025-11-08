1ee393923f8388daa4f5db54ab9d6a8deffbb1f4
Raj Saraswat <rajsarswata1@gmail.com>
2025-11-08 18:57:31 +0530

    Fix User model path for Render

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Signup route (improved duplicate handling)
 router.post("/signup", async (req, res) => {
