e7104c29b70ce2b99b7d79dd8ab4c7605567fbb0
Raj Saraswat <rajsarswata1@gmail.com>
2025-11-08 19:17:46 +0530

    Fix model path & passport config

--- passportConfig.js
+++ passportConfig.js
@@ -1,41 +1,9 @@
-// passportConfig.js
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("./models/User");
-require("dotenv").config();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const email = profile.emails && profile.emails[0] && profile.emails[0].value;
-        if (!email) return done(new Error("Google account has no email"), null);
-
-        let user = await User.findOne({ email });
-
-        if (!user) {
-          user = new User({
-            username: profile.displayName || email.split("@")[0],
-            email,
-            password: "google-auth" // dummy password for google signup
-          });
-          await user.save();
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err, null);
-      }
-    }
-  )
-);
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
@@ -46,3 +14,31 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails?.[0]?.value || "NoEmail",
+            avatar: profile.photos?.[0]?.value || "",
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
