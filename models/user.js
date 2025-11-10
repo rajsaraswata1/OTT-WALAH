ca83cdf603361c4cc8f7271d3fedf5ecd46b2472
Raj Saraswat <rajsarswata1@gmail.com>
2025-11-10 18:24:08 +0530

    Fix: correct model path user.js

--- models/user.js
+++ models/user.js
@@ -9,4 +9,4 @@ const userSchema = new mongoose.Schema({
   orders: { type: Array, default: [] }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema);
