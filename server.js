63135f37e51eb0ff5efa1c8da36d9aa191a69dcc
Raj Saraswat <rajsarswata1@gmail.com>
2025-11-15 10:03:46 +0530

    Fix session cookie and optimize server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production", // true in production
-     : 1000 * 60 * 60 * 24, // 1 day
+      60 * 60 * 24, // 1 day
     },
   })
 );
