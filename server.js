527a19696ec1b4f9888d6617388954d6816df0f1
Raj Saraswat <rajsarswata1@gmail.com>
2025-11-15 20:51:18 +0530

    Fixed uploads static path

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 const app = express();
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
